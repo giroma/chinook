adc7085a514d33afa4f682498df661599b7f5b28
Argir Argirov <dj.giro@gmail.com>
2017-10-14 12:57:51 -0400

    stretch completed

--- active_record_querries.rb
+++ active_record_querries.rb
@@ -17,3 +17,19 @@ Artist.order(:created_at).first(2)
 Track.where(genre_id: 15).order(:unit_price).first
 
 Track.where(genre_id: Genre.find_by(name: 'Electronica/Dance'), media_type_id: MediaType.where(name: 'MPEG audio file').first)
+
+Album.where("title LIKE ?", 'B%' )
+
+Artist.where("name LIKE ?", 'A%' )
+
+extra:
+
+select title from albums where artist_id = 51;
+
+select count(*) from tracks where media_type_id = 3;
+
+select name, unit_price from tracks where genre_id = 15 order by unit_price ASC LIMIT 1;
+
+select name from artists where name LIKE 'A%';
+
+select * from playlists_tracks where playlist_id = 1;
